ab8433a0dd24a0f1fe6c8ec0b9452e7447a50a2d
Daniel Dinev <daniel.dinev@scalefocus.com>
2020-04-26 04:37:09 +0300

    fix errors

--- ngclient/src/app/components/landing-page/landing-page.component.ts
+++ ngclient/src/app/components/landing-page/landing-page.component.ts
@@ -1,118 +1,95 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
 import { Tour } from 'src/app/models/Tour';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements AfterViewInit {
+export class LandingPageComponent implements AfterViewInit,OnInit {
 
-  searchForm: FormGroup;
-  searchField: FormControl;
-  @ViewChild('mapTour', {static: false}) mapRef: ElementRef;
   serverUrl = environment.serverUrl;
   map: google.maps.Map;
   tourName = '';
   tourDate: Date = new Date();
   markedLocation: google.maps.LatLng;
-
-  constructor(private http: HttpClient, private router: Router,private formBuilder: FormBuilder) { }
- ngOnInit():void{
-  this.searchForm = this.formBuilder.group({
-    search:[""]
-  });
- }
+  searchForm:FormGroup;
+  constructor(private http: HttpClient, private router: Router,private formBuilder:FormBuilder) { }
+  ngOnInit():void{
+    this.searchForm = this.formBuilder.group({
+      search:['']
+    });
+  }
   ngAfterViewInit(): void {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    this.map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -33.8688, lng: 151.2195},
-      zoom: 13
-    });
-    var card = document.getElementById('pac-card');
-    var input = document.getElementById('pac-input') as HTMLInputElement;
-    var types = document.getElementById('type-selector');
-    var strictbounds = document.getElementById('strict-bounds-selector')  as HTMLInputElement;
-
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
-
-    var autocomplete = new google.maps.places.Autocomplete(input);
-
-    // Bind the map's bounds (viewport) property to the autocomplete object,
-    // so that the autocomplete requests use the current map bounds for the
-    // bounds option in the request.
-    autocomplete.bindTo('bounds', map);
-
-    // Set the data fields to return when the user selects a place.
-    autocomplete.setFields(
-        ['address_components', 'geometry', 'icon', 'name']);
-
-    var infowindow = new google.maps.InfoWindow();
-    var infowindowContent = document.getElementById('infowindow-content');
-    infowindow.setContent(infowindowContent);
-    var marker = new google.maps.Marker({
-      map: map,
-      anchorPoint: new google.maps.Point(0, -29)
+      zoom: 13,
+      mapTypeId: 'roadmap'
     });
 
-    autocomplete.addListener('place_changed', function() {
-      infowindow.close();
-      marker.setVisible(false);
-      var place = autocomplete.getPlace();
-      if (!place.geometry) {
-        // User entered the name of a Place that was not suggested and
-        // pressed the Enter key, or the Place Details request failed.
-        window.alert("No details available for input: '" + place.name + "'");
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById('pac-input') as any;
+    const searchBox = new google.maps.places.SearchBox(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    this.map.addListener('bounds_changed', () => {
+      searchBox.setBounds(this.map.getBounds());
+    });
+
+    let markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', () => {
+      const places = searchBox.getPlaces();
+
+      if (places.length === 0) {
         return;
       }
 
-      // If the place has a geometry, then present it on a map.
-      if (place.geometry.viewport) {
-        map.fitBounds(place.geometry.viewport);
-      } else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(17);  // Why 17? Because it looks good.
-      }
-      marker.setPosition(place.geometry.location);
-      marker.setVisible(true);
-
-      var address = '';
-      if (place.address_components) {
-        address = [
-          (place.address_components[0] && place.address_components[0].short_name || ''),
-          (place.address_components[1] && place.address_components[1].short_name || ''),
-          (place.address_components[2] && place.address_components[2].short_name || '')
-        ].join(' ');
-      }
-
-      infowindowContent.children['place-icon'].src = place.icon;
-      infowindowContent.children['place-name'].textContent = place.name;
-      infowindowContent.children['place-address'].textContent = address;
-      infowindow.open(map, marker);
-    });
-
-    // Sets a listener on a radio button to change the filter type on Places
-    // Autocomplete.
-    function setupClickListener(id, types) {
-      var radioButton = document.getElementById(id);
-      radioButton.addEventListener('click', function() {
-        autocomplete.setTypes(types);
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
       });
-    }
+      markers = [];
 
-    setupClickListener('changetype-all', []);
-    setupClickListener('changetype-address', ['address']);
-    setupClickListener('changetype-establishment', ['establishment']);
-    setupClickListener('changetype-geocode', ['geocode']);
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+      places.forEach((place) => {
+        if (!place.geometry) {
+          console.log('Returned place contains no geometry');
+          return;
+        }
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
 
-   
-  strictbounds.addEventListener('click', function() {
-          console.log('Checkbox clicked! New state=' + this.checked);
-          autocomplete.setOptions({strictBounds: this.checked});
-        });
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: this.map,
+          icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        this.markedLocation = place.geometry.location;
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      this.map.fitBounds(bounds);
+    });
   }
 
   createTour() {
@@ -125,16 +102,15 @@ export class LandingPageComponent implements AfterViewInit {
       startDateTime: this.tourDate,
     };
 
-     if (this.tourName !== '') {
-     this.http.post(this.serverUrl + 'api/tour', tourToCreate).toPromise().then((tour: Tour) => {
-     if (tour) {
-        this.navigateToTour(tour.tourHash);
+    if (this.tourName !== '') {
+      this.http.post(this.serverUrl + 'api/tour', tourToCreate).toPromise().then((tour: Tour) => {
+        if (tour) {
+          this.navigateToTour(tour.tourHash);
         }
       }).catch((err: HttpErrorResponse) => {
-         console.log('Creating tour failed.', err);
-       });
-     }
-     
+        console.log('Creating tour failed.', err);
+      });
+    }
   }
 
   private navigateToTour(hash: any) {
