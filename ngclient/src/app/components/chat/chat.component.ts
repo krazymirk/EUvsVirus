a79610c5ef3051bd472a271d0d638ed2191ac500
Nikola Aleksandrov <nikola.aleksandrov@scalefocus.com>
2020-04-26 05:02:12 +0300

    Updated chat component

--- ngclient/src/app/components/chat/chat.component.ts
+++ ngclient/src/app/components/chat/chat.component.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+interface Message {
+  name: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
-  
-  @Output() message = new EventEmitter<string>()
 
+export class ChatComponent implements OnInit {
+
+  public heads = [
+    {
+      name: 'John Doe',
+      message: 'false'
+    },
+    {
+      name: 'Bohn Boe',
+      message: 'false'
+    },
+    {
+      name: 'Nikola A',
+      message: 'false'
+    },
+    {
+      name: 'D G',
+      message: 'false'
+    },
+    {
+      name: 'John Doe',
+      message: 'false'
+    },
+    {
+      name: 'Bohn Boe',
+      message: 'false'
+    }
+];
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  ngOnSendMessage(message):void{
-    this.message.emit(message)
+  public setmessage(m: Message){
+    const exst = this.heads.find(h => h.name === m.name);
+    if (!exst) {
+     this.heads.push(m);
+    } else {
+      exst.message = m.message;
+    }
   }
 }
